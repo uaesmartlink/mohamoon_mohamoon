88b8958931611f07618ba2317aa331f51dbe51fe
uaesmartlink <uaesmartlink@gmail.com>
2023-02-13 19:38:58 +0400

    add logo

--- lib/app/modules/login/views/widgets/title_widget.dart
+++ lib/app/modules/login/views/widgets/title_widget.dart
@@ -2,32 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 Widget titleApp() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      /*Image.asset(
-        'assets/icons/ic_launcher.png',
-        width: 45,
-        height: 45,
+  // return Row(
+  //   mainAxisAlignment: MainAxisAlignment.center,
+  //   children: [
+  //     /*Image.asset(
+  //       'assets/icons/ic_launcher.png',
+  //       width: 45,
+  //       height: 45,
+  //     ),
+  //     SizedBox(
+  //       width: 10,
+  //     ),*/
+  //     RichText(
+  //       textAlign: TextAlign.center,
+  //       text: TextSpan(
+  //           text: 'Mohamoon for'.tr,
+  //           style: TextStyle(
+  //               fontSize: 30,
+  //               fontWeight: FontWeight.w700,
+  //               color: Colors.white,),
+  //           children: [
+  //             TextSpan(
+  //               text: ' Lawyer'.tr,
+  //               style: TextStyle(color: Colors.white, fontSize: 30),
+  //             ),
+  //           ]),
+  //     ),
+  //   ],
+  // );
+  return Container(
+    height: 100,
+    width: 220,
+    decoration: const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage('assets/images/logo.png'),
+        fit: BoxFit.fill,
       ),
-      SizedBox(
-        width: 10,
-      ),*/
-      RichText(
-        textAlign: TextAlign.center,
-        text: TextSpan(
-            text: 'Mohamoon for'.tr,
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,),
-            children: [
-              TextSpan(
-                text: ' Lawyer'.tr,
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ]),
-      ),
-    ],
+    ),
   );
 }
